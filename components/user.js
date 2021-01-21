043322e6243e6af3dc7b6878c73c3f10c5052f67
grafijoss <grafijoss@hotmail.com>
2021-01-20 21:48:19 -0500

    add events

--- components/user.js
+++ components/user.js
@@ -1,10 +1,30 @@
 import { Component, createElement } from "../lib/react/index.js";
 
 class User extends Component {
+  // recibe las propiedades cuando se instancia la clase
+  constructor(props) {
+    // hace que todo this este disponible en la clase superior (Component)
+    super(props);
+
+    // asigna un nuevo this a la funcion
+    // retorna una nueva funcion
+    // asigamos la nueva funcion a this.handleClick
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  // event-lesson
+  handleClick(event) {
+    console.table(this.props);
+  }
+  handleClickArrow = (event) => {
+    console.table(this.props);
+  };
   render() {
     const { avatar, name } = this.props;
 
     return createElement("div", {
+      // event-lesson
+      onClick: this.handleClickArrow,
       class: "user",
       children: [
         createElement("div", {
